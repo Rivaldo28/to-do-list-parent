b9310a6604fa70684850bfe39a7c5af4729d1667
Rivaldo Souza <rivaldo.lsouza@outlook.com>
2024-10-13 17:48:26 -0300

    validation and testing 2

--- to-do-list-api/src/test/java/com/example/demo/ToDoListApiApplicationTests.java
+++ to-do-list-api/src/test/java/com/example/demo/ToDoListApiApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import com.lista.tarefas.ToDoListApiApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = ToDoListApiApplication.class)
 class ToDoListApiApplicationTests {
 
 	@Test
